0d2f7d83da6349d70f84d3d8bf4314cae9276ead
Rishabh <rishabhmadan17@gmail.com>
2020-01-15 00:26:11 +0530

    Made changed to the Frontier and modified the code to use Pipeline instead of redis directly

--- core/Frontier.py
+++ core/Frontier.py
@@ -1,6 +1,7 @@
 import redis
 import re
 import requests
+from Pipeline import Pipeline
 
 REDIS_HOST = 'localhost'
 REDIS_PORT = 6379
@@ -11,57 +12,61 @@ rhref = re.compile(r'<[aA].*(href|HREF)=([^\s>]+)')
 
 
 class Frontier():
-    def __init__(self, seed_key="seed_urls", crawl_frontier='todo'):
+    def __init__(self, seed_key="seed_urls"):
         self.seed_key = seed_key
-        self.crawl_frontier = crawl_frontier
+        self.crawl_frontier = []
 
     def add_seed(self, seed_urls):
         r.sadd(self.seed_key, *set(seed_urls))
+        for url in seed_urls:
+            self.crawl_frontier.append(Pipeline(name=url, redis_client=r))
 
     def start_seeding(self):
-        url_list = list(map(lambda x: x.decode(
-            'utf-8'), r.smembers(self.seed_key)))
-
-        for url in url_list:
-            res = requests.get(url)
+        for pipeline in self.crawl_frontier:
+            res = requests.get(pipeline.name)
             links = rhref.findall(res.text)
-
+            print(links)
             for link in links:
                 link = link[1].replace("\'", '').replace('"', "").split("#")[0]
                 if link.startswith("mailto:"):
                     continue
-                if link[:4] == 'http':
-                    if link.startswith(url):
-                        r.lpush(self.crawl_frontier, link)
+                elif link[:4] == 'http':
+                    if link.startswith(pipeline.name):
+                        # r.lpush(self.crawl_frontier, link)
+                        pipeline.push([link])
                 else:
                     if link.startswith('/'):
-                        r.lpush(self.crawl_frontier, url.rstrip('/') + link)
+                        # r.lpush(self.crawl_frontier,
+                        #         pipeline.name.rstrip('/') + link)
+                        pipeline.push([pipeline.name.rstrip('/') + link])
                     else:
-                        r.lpush(self.crawl_frontier,
-                                url.rstrip('/') + '/' + link)
+                        # r.lpush(self.crawl_frontier,
+                        #         pipeline.name.rstrip('/') + '/' + link)
+                        pipeline.push([pipeline.name.rstrip('/') + '/' + link])
 
     def get_seed(self):
         return list(map(lambda x: x.decode(
             'utf-8'), r.smembers(self.seed_key)))
 
     def remove_seed(self, seed_urls):
+        for pipeline in self.crawl_frontier:
+            if pipeline.name in seed_urls:
+                pipeline.flush()
         return r.srem(self.seed_key, *set(seed_urls))
 
     def get_frontier(self):
-        pass
+        return self.crawl_frontier
 
     def flush_frontier(self):
-        pass
+        for pipeline in self.crawl_frontier:
+            pipeline.flush()
+
+        return 'flushed'
 
 
 frontier = Frontier()
-
+frontier.flush_frontier()
 frontier.add_seed(['https://news.ycombinator.com/',
                    'https://blog.rishabhmadan.com'])
-
 print(frontier.get_seed())
-
-frontier.remove_seed(['https://news.ycombinator.com/',
-                      'https://blog.rishabhmadan.com'])
-
-print(frontier.get_seed())
+frontier.start_seeding()
--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,3 @@
 __author__ = 'Rishabh Madan'
 
-from .Fetcher import Fetcher
-from .Frontier import Frontier
 from .Pipeline import Pipeline
